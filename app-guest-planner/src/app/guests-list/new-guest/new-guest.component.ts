7a756e80bd09a54d820828b3bed231b8f4b3bb6e
Vanina Stancheva <vanina.v.stancheva@gmail.com>
2024-12-17 19:15:19 +0200

    creating a modal

--- app-guest-planner/src/app/guests-list/new-guest/new-guest.component.ts
+++ app-guest-planner/src/app/guests-list/new-guest/new-guest.component.ts
@@ -1,4 +1,4 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component, Output, EventEmitter, Input} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 @Component({
@@ -12,6 +12,11 @@ import {FormsModule} from '@angular/forms';
 export class NewGuestComponent {
 enteredName: string = '';
 @Output() addName = new EventEmitter<string>();
+@Output() close = new EventEmitter<void>();
+
+  onCancel() {
+    this.close.emit();
+  }
 
 onSubmit() {
   this.addName.emit(this.enteredName);
